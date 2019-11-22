1883699807f01869296e5e3338ad410ef220f577
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2019-11-22 14:12:11 -0800

    cleanup

---
+++ module2-oop-code-style-and-reviews/2.Assignment/setup.py
@@ -0,0 +1,32 @@
+"""
+lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "sklearn"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="Lambdata_CVanchieri",
+    version="0.0.1",
+    author="Charles Vanchieri",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://lambdaschool.com/courses/data-science/",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
